1711a39d67588d61ac28afb1ccedaffaae3a17e6
bobobyk <mykha07@mail.ru>
2023-11-12 04:18:44 +0300

    dgdfgdfg
    dfgdfgdfg

---
+++ jj.py
@@ -0,0 +1,24 @@
+# A
+
+# import random
+# a = [random.randint(0, 10) for a in range(6)]
+# print(a)
+# print(len(set(a)))
+
+#B
+
+# import random
+
+# a = set([random.randint(0, 10) for a in range(6)])
+# b = set([random.randint(0, 10) for b in range(6)])
+# print(a)
+# print(b)
+
+# count = 0
+# for i in a:
+#     for j in b:
+#         if i == j:
+#             count +=1
+# print(count)
+
+
